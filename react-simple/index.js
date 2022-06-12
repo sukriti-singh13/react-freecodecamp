7ff37e876a47949ca332adcf36573c3cd19355ce
sukriti-sigh13 <sukriti.singh899@gmail.com>
2022-06-12 21:54:48 +0530

    first changes

---
+++ react-simple/index.js
@@ -0,0 +1,15 @@
+
+const element =(<nav>
+<h1>Brand Name</h1>
+<ul>
+<li>About</li>
+<li>contact</li>
+</ul>
+</nav>)
+console.log(element)
+
+
+
+ReactDOM.render(
+element
+,document.getElementById("root")) 
